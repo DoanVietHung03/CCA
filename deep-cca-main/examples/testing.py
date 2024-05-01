5d810cd25ad81c6e81de7bb8d847a4883a2da470
hung <hungdoan29102003@gmail.com>
2024-05-01 18:30:49 +0700

    "update"

--- deep-cca-main/examples/testing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-test = np.load("C:\\Users\\ACER\\OneDrive\\Desktop\\MSA\\PROJECT\\DCCA\\deep-cca-main\\examples\\new_features.npy", allow_pickle=True)
-
-test_data, test_pred, test_label = test[2]
-
-plt.plot(test_data[:,0], test_pred[:,0], 'ro')
-plt.show()
